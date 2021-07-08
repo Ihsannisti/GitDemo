0e0e042c61169da7b0ba1cb02d0e285fe81f1c00
ihsan <mi.muhammadihsan19@gmail.com>
2021-07-08 10:56:20 +0700

    first commit

---
+++ src/test/java/MavenTutorial/MavenJava/APITest.java
@@ -0,0 +1,21 @@
+package MavenTutorial.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("Post Jira");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("Delete Twitter");
+	}
+
+}
---
+++ src/test/java/MavenTutorial/MavenJava/AppiumTest.java
@@ -0,0 +1,19 @@
+package MavenTutorial.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAndroid()
+	{
+		System.out.println("Native Android");
+	}
+	
+	@Test
+	public void iOSApps()
+	{
+		System.out.println("iOS Apps");
+	}
+
+}
